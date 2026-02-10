716aa74591d205e841d42e549f0de7fe44f5c72e
lethuong2k3 <leanhthuong2k3@gmail.com>
2026-02-10 20:54:53 +0700

    Migrate_Tailwind_from_CDN_to_Build_Process

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
+import './index.css';
 import App from './App';
 
 const queryClient = new QueryClient({
